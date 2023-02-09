ca501da4c72cd4ddbc5ad1076eaf90d4419a6468
OlirahetaX <115906752+OlirahetaX@users.noreply.github.com>
2023-02-09 16:40:28 -0600

    Add files via upload

---
+++ Entrega_ejercicios_tema1/src/entrega_ejercicios_tema1/Entrega_ejercicios_tema1.java
@@ -0,0 +1,21 @@
+
+package entrega_ejercicios_tema1;
+
+
+public class Entrega_ejercicios_tema1 {
+
+
+    public static void main(String[] args) {
+        int intt = 5;
+        long longg = 54;
+        double doublee = 5.4;
+        boolean boo = true;
+        String string= "Hola mundo";
+        System.out.println(intt);
+        System.out.println(longg);
+        System.out.println(doublee);
+        System.out.println(boo);
+        System.out.println(string);
+    }
+    
+}
